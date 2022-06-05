53dededa6405f73451f1740546397447971cdef3
adam <adsyahir16@gmail.com>
2022-06-06 01:20:09 +0800

    adam

--- navigation/screens/MedCabinet.js
+++ navigation/screens/MedCabinet.js
@@ -1,6 +1,4 @@
 import {StyleSheet, View} from 'react-native';
-import { launchCamera, launchImageLibrary } from "react-native-image-picker";
-import RNTextDetector from "rn-text-detector";
 import {
   Input,
   Select,
@@ -14,31 +12,51 @@ import {
   useTheme,
 } from "@ui-kitten/components";
 import React, { useEffect, useState, useRef } from "react";
+import firestore from '@react-native-firebase/firestore';
 
 export default function MedCabinet() {
-  
+  let [userId, setUserId] = useState('Paracetamol');
+  let [userData, setUserData] = useState({});
+ 
+  const searchUser = () => {
+    if (userId) {
+      firestore()
+        .collection('Medicines')
+        .doc(userId)
+        .get()
+        .then((documentSnapshot) => {
+          /*
+            A DocumentSnapshot belongs to a specific document,
+            With snapshot you can view a documents data,
+            metadata and whether a document actually exists.
+          */
+          let userDetails = {};
+          // Document fields
+          userDetails = documentSnapshot.data();
+          // All the document related data
+          userDetails['id'] = documentSnapshot.id;
+          setUserData(userDetails);
+        });
+    }
+  };
   return (
-    <View style={styles.container}>
-      <View style={styles.row}>
-      <Input
-          style={styles.input}
-          label="Medicine Name"
-          placeholder="Enter Medicine Name"
-          size="medium"
-
-        /> 
-         <Button
-          size="small"
-          appearance="outline"
-          status="primary"
-        >OCR </Button>
-        <Button
-          size="small"
-          appearance="outline"
-          status="primary"
-        >Search </Button>
-      </View>
+    <View style={{paddingHorizontal: 35}}>
+    <Input
+      placeholder="Enter User Id"
+      onChangeText={(userId) => setUserId(userId)}
+      value={userId}
+      style={{padding: 10}}
+    />
+    <Button  onPress={searchUser}>Search User</Button>
+    <View style={{marginTop: 10}}>
+      <Text>
+        Medicine Name: {userData ? userData.id : ''}
+      </Text>
+      <Text>
+        Medication Indication: {userData ? userData.indication : ''}
+      </Text>
     </View>
+  </View>
   );
 }
 const styles = StyleSheet.create({
